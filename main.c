750b54b522f0fcf32acd950a1596c5de198d3ba5
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-08-15 00:32:59 -0300

    Implementadas as funções incompletas

--- main.c
+++ main.c
@@ -1,18 +1,8 @@
-// ED 2022 SI2/CC2
-// Segunda versão do jogo wordle
-// Visto em aula dia 2mai
-// Comentários adicionados dia 7mai
-//
-// Implementado em abordagem descendente (top-down)
-// Com um "dicionário" escondendo a implementação do conjunto de palavras
-//
-// Algumas funções não estão implementadas (exceto para que compile)
-// Implemente o que falta para que o jogo funcione corretamente.
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 
 // -----------------------------------------------------------------------------------------
@@ -23,54 +13,139 @@ typedef struct {
     bool usada;
 } palavra_t;
 
-
-
 // -----------------------------------------------------------------------------------------
 // tipo de dados para o dicionário (está oculto e a implementação não funciona muito)
-// **falta definir a "struct dicionario"**, com os dados necessários para manter um dicionário de
-// palavras e implementar as funções abaixo
 typedef struct dicionario dicionario_t;
+
+struct dicionario {
+    char* nome_arquivo;
+    int n_palavras;
+    palavra_t* palavras;
+};
+
+// Converte o segundo byte de uma caractere acentuado em UTF-8 para o caractere não acentuado correspondente
+// Função BEM meia boca, só funciona para os caracteres selecionados, existentes no arquivo de palavras.
+char tira_acento(unsigned char c)
+{
+    unsigned char ac[] = "áâãéêíóôõúç";
+    char sem_acento[]  = "aaaeeiooouc";
+    for (int i=0; ac[i] != '\0'; i++) {
+        if (c == ac[i]) return sem_acento[i/2];
+    }
+    printf("Não sei converter o caractere %d (%c%c)\n", c, 195, c);
+    return c;
+}
+
+bool palavra_compara(char palavra[5], char* entrada) {
+    for(int i=0; i<5; i++) {
+        if(palavra[i] != entrada[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// lê a próxima palavra do arquivo; retorna true se sucesso
+// No arquivo tem uma palavra acentuada (5 caracteres, codificados em até 10 bytes),
+//   opcionalmente seguido por um ponto (para indicar que já foi usada),
+//   obrigatoriamente seguido por um fim de linha.
+bool le_palavra(FILE *arquivo, palavra_t *palavra)
+{
+    int c;
+    int n = 0;
+    int nsa = 0;
+    palavra->usada = false;
+    for (;;) {
+        c = fgetc(arquivo);
+        if (c == '\r') continue;
+        if (c == EOF) return false;
+        if (c == '\n') {
+            break;
+        }
+        if (c == '.') {
+            palavra->usada = true;
+            continue;
+        }
+        if (n >= 10) {
+            return false;
+        }
+        palavra->com_acento[n] = c;
+        n++;
+        if (c >= 'a' && c <= 'z') {
+            palavra->sem_acento[nsa++] = (char)c;
+        } else {
+            if (c != 195) {
+                palavra->sem_acento[nsa++] = tira_acento(c);
+            }
+        }
+    }
+    if (n < 10) {
+        palavra->com_acento[n] = '\0';
+    }
+    return true;
+}
 
 // aloca memória para um dicionário de palavras, inicializa ele com as palavras lidas do
 // arquivo com nome "nome_arquivo"; retorna um ponteiro para o dicionário criado
-// **essa função não está completa**
 dicionario_t *dicionario_cria(char *nome_arquivo)
 {
-  return NULL;
+    FILE *arquivo = fopen(nome_arquivo, "r");
+    if(arquivo == NULL) {
+        printf("Falha ao abrir o arquivo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    dicionario_t *dic = malloc(sizeof(dicionario_t));
+    dic->nome_arquivo = strdup(nome_arquivo);
+
+    dic->n_palavras = 0;
+    char ch = '\0';
+    while (ch != EOF) {
+        ch = (char)fgetc(arquivo);
+        if(ch == '\n') {
+            dic->n_palavras++;
+        }
+    }
+
+    rewind(arquivo);
+    dic->palavras = malloc(sizeof(palavra_t) * dic->n_palavras);
+
+    palavra_t palavra;
+    for(int i=0; i<dic->n_palavras; i++) {
+        assert(le_palavra(arquivo, &palavra));
+        dic->palavras[i] = palavra;
+    }
+    fclose(arquivo);
+    return dic;
 }
 // libera a memória ocupada pelo dicionário "dic"
-// **essa função não está completa**
 void dicionario_destroi(dicionario_t *dic)
 {
+    free(dic->nome_arquivo);
+    free(dic->palavras);
+    free(dic);
 }
-// retorna o nome do arquivo de onde foram lidas as palavras que estão no dicionário "dic"
-// **essa função não está completa**
+
+// retorna o nome do arquivo usado pelo dicionário
 char *dicionario_nome_arquivo(dicionario_t *dic)
 {
-  return "oi";
+  return dic->nome_arquivo;
 }
 // retorna o número de palavras existentes no dicionário "dic"
-// **essa função não está completa**
 int dicionario_numero_de_palavras(dicionario_t *dic)
 {
-  return 2;
+  return dic->n_palavras;
 }
-// dado criado somente para fazer de conta que tem palavras, e o programa ter um funcionamento mínimo
-palavra_t p[] = {{ "teste", "teste", false }, { "terés", "teres" }};
-// retorna um ponteiro para a palavra que está na posição "pos" do dicionário "dic"
-// **essa função não está completa**
+
 palavra_t *dicionario_palavra_na_posicao(dicionario_t *dic, int pos)
 {
-  return &p[pos];
+  return &dic->palavras[pos];
 }
-
-
 
 // -----------------------------------------------------------------------------------------
 // tipo de dados para as dicas sobre uma letra
 typedef enum {inexistente, trocado, certo} classe_letra_t;
-
-
 
 // -----------------------------------------------------------------------------------------
 // funções de E/S na tela
@@ -104,7 +179,7 @@ void escreve_palavra(FILE *arq, palavra_t *p)
 
 // escreve na tela a versão com acentos da palavra apontada por "chute", colorindo cada letra de
 //   acordo com a classificação dela no vetor "cl"
-void desenha_palavra(palavra_t *chute, classe_letra_t cl[5])
+void desenha_palavra(palavra_t *chute, const classe_letra_t cl[5])
 {
   int jj = 0;
   for (int l=0; l<3; l++) {
@@ -137,20 +212,35 @@ void desenha_palavra(palavra_t *chute, classe_letra_t cl[5])
 // lê uma palavra do usuário,
 //   até que seja uma palavra pertencente ao dicionário (e retorna um ponteiro para essa palavra no dicionário)
 //   ou que seja a palavra especial que indica a desistência do usuário (nesse caso, retorna NULL)
-// **essa função não está completa**
 palavra_t *le_chute(dicionario_t *dic)
 {
-  return dicionario_palavra_na_posicao(dic, 1);
+    char input[64];
+    while(true) {
+        printf("Digite a palavra (fim para desistir): ");
+        fgets(input, sizeof(input) - 2, stdin);
+        fflush(stdin);
+        input[strlen(input)-1] = '\0';
+        if(strcmp(input, "fim") == 0) {
+            exit(EXIT_SUCCESS);
+        }
+        for(int i=0; i<dic->n_palavras;i++) {
+            if(palavra_compara(input, dic->palavras[i].sem_acento)) {
+                return dicionario_palavra_na_posicao(dic, i);
+            }
+        }
+        printf("\nPalavra não encontrada no dicionário! Tente novamente...\n");
+    }
 }
 
 // pergunta ao usuário se ele quer jogar mais uma partida e retorna true caso positivo
-// **essa função não está completa**
 bool quer_jogar_de_novo(void)
 {
-  return false;
+    char input[3];
+    printf("Deseja jogar novamente (S/N) ? ");
+    fgets(input, sizeof(input) - 1, stdin);
+    fflush(stdin);
+    return(input[0] == 'S' || input[0] == 's');
 }
-
-
 
 // -----------------------------------------------------------------------------------------
 // esta função tá meio peixe fora d'água (deveria ter sido incluida na interface do dicionário!)
@@ -241,7 +331,7 @@ bool joga(dicionario_t *palavras)
 int main()
 {
   dicionario_t *palavras;
-  palavras = dicionario_cria("palavras_de_5_letras");
+  palavras = dicionario_cria("palavras_de_5_letras.txt");
   
   do {
     joga(palavras);
